09e312fdd1736de6c1ecb9f2f696fa6495408fb9
gabriellehora <gabrielle_hora@estudante.sesisenai.org.br>
2026-07-02 15:40:04 -0300

    Delete SA - zenstudy directory

--- SA - zenstudy/bd-scripts/script-create-agenda.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE tb_agenda (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    titulo VARCHAR(100) NOT NULL,
-    descricao TEXT,
-    horario TIME NOT NULL,
-    dt_data DATE NOT NULL
-);
--- SA - zenstudy/bd-scripts/script-create-tb_cadastro.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE tb_cadastro(
-  id INT AUTO_INCREMENT,
-  nm_usuario VARCHAR(100),
-  ds_senha VARCHAR(30),
-  ds_email VARCHAR(80) NOT NULL,
-  CONSTRAINT pk_usuario PRIMARY KEY(id)
-);
