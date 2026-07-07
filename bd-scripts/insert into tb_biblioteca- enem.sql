71b5d931e526d57492564d3dcdaa7da72c51acdd
gabriellehora <gabrielle_hora@estudante.sesisenai.org.br>
2026-07-07 16:35:09 -0300

    Delete bd-scripts directory

--- bd-scripts/insert into tb_biblioteca- enem.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO tb_biblioteca (titulo, descricao, materia, ano) VALUES
-('Redação ENEM', 'Estrutura dissertativa argumentativa.', 'Redação', 'ENEM'),
-('Matemática ENEM', 'Funções, estatística e geometria.', 'Matemática', 'ENEM'),
-('Ciências Humanas ENEM', 'História, Geografia, Filosofia e Sociologia.', 'Ciências Humanas', 'ENEM'),
-('Ciências da Natureza ENEM', 'Física, Química e Biologia.', 'Ciências da Natureza', 'ENEM'),
-('Interpretação de Texto ENEM', 'Leitura e compreensão textual.', 'Português', 'ENEM'),
-('Atualidades ENEM', 'Temas contemporâneos.', 'Geral', 'ENEM');
